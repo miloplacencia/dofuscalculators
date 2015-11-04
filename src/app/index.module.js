a62796e99bcc1db3b670d23b29a06761d79a9cfe
milo <placenciameister@gmail.com>
2015-11-03 21:47:23 -0300

    new branch!

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('dofuscalculators2', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
